b4ad5cd5ce195aded9fcec798cd0a86ac5c83dc0
degiwhlo2co <147834857+degiwhlo2co@users.noreply.github.com>
2024-05-07 16:05:52 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+/*
+    SETUP
+*/
+// Express
+var express = require('express');   // We are using the express library for the web server
+var app     = express();            // We need to instantiate an express object to interact with the server in our code
+PORT        = 3790;                 // Set a port number at the top so it's easy to change in the future
+
+// Database
+//var db = require('./db-connector')
+
+
+/*
+    ROUTES
+*/
+app.use(express.static('public/index.html'));
+
+/*
+    LISTENER
+*/
+app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
+    console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
+});
