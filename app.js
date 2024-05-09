96917ee78ca63230a3ad5cea65a8b78591d3f2ae
degiwhlo2co <147834857+degiwhlo2co@users.noreply.github.com>
2024-05-08 20:57:36 -0400

    Port Number 3791 for app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 // Express
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT        = 3790;                 // Set a port number at the top so it's easy to change in the future
+PORT        = 3791;                 // Set a port number at the top so it's easy to change in the future
 
 // Database
 //var db = require('./db-connector')
