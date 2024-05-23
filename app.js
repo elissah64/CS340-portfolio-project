5136886b841ec390d70e1067f51b27d2f49c66fc
degiwhlo2co <147834857+degiwhlo2co@users.noreply.github.com>
2024-05-23 15:06:30 -0400

    Update app.js- most current, delete and read work

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
 // Date: 5/22/2024
 // Based on:
 // Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app
-
+/*
+    SETUP
+*/
 // Express
-var express = require('express');   // We are using the express library for the web server
-var app     = express();           // We need to instantiate an express object to interact with the server in our code
-PORT        = 3791;                 // Set a port number at the top so it's easy to change in the future
-
+var express = require('express');
+var app = express();
+PORT = 3790;
+const path = require('path');
 // Database
 var db = require('./database/db-connector');
 
@@ -15,54 +17,87 @@ var db = require('./database/db-connector');
 const { engine } = require('express-handlebars');
 var exphbs = require('express-handlebars');     // Import express-handlebars
 app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
-app.set('view engine', '.hbs');
+app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
 
+// app.js
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-app.use(express.static('public'));
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 /*
     ROUTES
 */
-
+// Display Home Page
 app.get('/', function(req, res)
     {
-        res.render('index');                    // Note the call to render() and not send(). Using render() ensures the templating engine
+        res.render('index');
     });
 
-    /*
-// Read
+// Display Patrons Page
 app.get('/patrons', function(req, res)
-    {
-        let query1 = "SELECT * FROM Patrons;";
-        db.pool.query(query1, function(error, rows, fields){
-            res.render('patrons', {data: rows});
-        })
+{
+    let query1 = "SELECT * FROM Patrons;";
+    db.pool.query(query1, function(error, results){
+        if (error) {
+        res.status(500).send('Database error: ' + error.message);
+        } else {
+        res.render('patrons', { data: results });
+        }
     });
+});
 
-// Delete
+// Display Transactions Page
+app.get('/transactions', function(req, res)
+{
+    let query1 = "SELECT * FROM Transactions;";
+    db.pool.query(query1, function(error, results){
+        if (error) {
+        res.status(500).send('Database error: ' + error.message);
+        } else {
+        res.render('transactions', { data: results });
+        }
+    });
+});
+
+//Display Books Page
+app.get('/books', function(req, res)
+{
+    let query1 = "SELECT * FROM Books;";
+    db.pool.query(query1, function(error, results){
+        if (error) {
+        res.status(500).send('Database error: ' + error.message);
+        } else {
+        res.render('books', { data: results });
+        }
+    });
+});
+
+// Display Holds table
+app.get('/holds', function(req, res)
+{
+        let query1 = "SELECT * FROM Holds;";
+    db.pool.query(query1, function(error, results){
+        if (error) {
+        res.status(500).send('Database error: ' + error.message);
+        } else {
+        res.render('holds', { data: results });
+        }
+    });
+});
+
 app.delete('/delete-hold/:holdID', function(req,res,next){
     let data = req.body;
-    let holdID = parseInt(data.id);
-    let deleteHold = `DELETE FROM Holds WHERE pid = holdID`;
-    let deletePatronHold= `DELETE FROM Patron_Holds WHERE id = holdID`;
+    console.log(req.params.holdID);
+    let deleteHold = `DELETE FROM Holds WHERE holdID = ?`;
 
-
-          // Run the 1st query
-          db.pool.query(deleteHold, [holdID], function(error, rows, fields){
+          db.pool.query(deleteHold, [req.params.holdID], function(error, rows, fields){
               if (error) {
-
-              // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
               console.log(error);
               res.sendStatus(400);
               }
-
               else
               {
-                  // Run the second query
-                  db.pool.query(deletePatronHold, [holdID], function(error, rows, fields) {
-
+                  db.pool.query(deleteHold, [req.params.holdID], function(error, rows, fields) {
                       if (error) {
                           console.log(error);
                           res.sendStatus(400);
@@ -71,14 +106,46 @@ app.delete('/delete-hold/:holdID', function(req,res,next){
                       }
                   })
               }
-  })});*/
+  })});
+
+  // app.js
+
+  app.post('/add-hold-form', function(req, res) {
+    let data = req.body;
+
+    // Construct the SQL query using placeholders
+    let query1 = `INSERT INTO Holds (name, title, datePlaced, status, notificationPref) VALUES (${name}, ${title}, ${datePlaced}, ${posInQueue}, ${status}, ${notificationPref})`;
+
+    // Execute the query
+    db.pool.query(query1, function(error, rows, fields) {
+        if (error) {
+            console.log(error);
+            res.sendStatus(400);
+        } else {
+            res.redirect('/Holds');
+        }
+    });
+});
 
 
 
+// Display Patron_Holds Page
+app.get('/patron_holds', function(req, res)
+{
+        let query1 = "SELECT * FROM Patron_Holds;";
+    db.pool.query(query1, function(error, results){
+        if (error) {
+        res.status(500).send('Database error: ' + error.message);
+        } else {
+        res.render('patron_holds', { data: results });
+        }
+    });
+});
 
 /*
     LISTENER
 */
-app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
+app.listen(PORT, function(){
     console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
 });
+
