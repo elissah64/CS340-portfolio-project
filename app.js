650dc7aced562cf0442fc457692781bb89fba6c1
degiwhlo2co <147834857+degiwhlo2co@users.noreply.github.com>
2024-05-28 18:16:59 -0400

    Update app.js with fully functional holds, patrons, and books CRUD

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 // Express
 const express = require('express');
 const app = express();
-const PORT = 3791;
+const PORT = 3792;
 const path = require('path');
 // Database
 const db = require('./database/db-connector');
@@ -27,13 +27,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 /*
     ROUTES
 */
-// Display Home Page
+// DISPLAY HOME
 app.get('/', function(req, res)
     {
         res.render('index');
     });
 
-// Display Patrons Page
+// DISPLAY PATRON TABLE
 app.get('/patrons', function(req, res)
 {
     let query1 = "SELECT * FROM Patrons;";
@@ -46,7 +46,79 @@ app.get('/patrons', function(req, res)
     });
 });
 
-// Display Transactions Page
+// DELETE PATRON
+app.delete('/delete-patron/:patronID', function(req,res,next){
+    let data = req.body;
+    let deletePatron = `DELETE FROM Patrons WHERE patronID = ?`;
+
+          db.pool.query(deletePatron, [req.params.patronID], function(error, rows, fields){
+              if (error) {
+              console.log(error);
+              res.sendStatus(400);
+              }
+              else
+              {
+                  db.pool.query(deletePatron, [req.params.patronID], function(error, rows, fields) {
+                      if (error) {
+                          console.log(error);
+                          res.sendStatus(400);
+                      } else {
+                          res.sendStatus(204);
+                      }
+                  })
+              }
+  })});
+
+// ADD PATRON
+app.post('/add-patron-form', function(req, res) {
+
+    let data = req.body;
+    let query1 = `INSERT INTO Patrons (patronID, name, address, email, phone) VALUES (?, ?, ?, ?, ?)`;
+    // Execute the query with parameter values
+    db.pool.query(query1, [data.patronID, data.name, data.address, data.email, data.phone], function(error, rows, fields) {
+        if (error) {
+            console.log(error);
+            res.sendStatus(400);
+        } else {
+            res.redirect('/patrons');
+        }
+    });
+});
+
+// UPDATE PATRON
+app.put('/update-patron-form', function(req,res,next){
+    let data = req.body;
+
+    let patronID = parseInt(data.patronID);
+    let name = data.name;
+    let address = data.address;
+    let email = data.email;
+    let phone = data.phone;
+
+    let queryUpdatePatron = `UPDATE Patrons SET name = ?, address = ?, email = ?, phone = ? WHERE patronID = ?`;
+
+          db.pool.query(queryUpdatePatron, [ name, address, email, phone, patronID], function(error, rows, fields){
+              if (error) {
+              console.log(error);
+              res.sendStatus(400);
+              }
+              else
+              {
+                let selectQuery = 'SELECT * FROM Patrons WHERE patronID = ?'
+                db.pool.query(selectQuery, [patronID], function(error, rows, fields) {
+
+                      if (error) {
+                          console.log(error);
+                          res.sendStatus(400);
+                      } else {
+                          res.send(rows);
+                      }
+                  })
+              }
+  })});
+
+
+// DISPLAY TRANSACTION TABLE
 app.get('/transactions', function(req, res)
 {
     let query1 = "SELECT * FROM Transactions;";
@@ -59,20 +131,48 @@ app.get('/transactions', function(req, res)
     });
 });
 
-//Display Books Page
-app.get('/books', function(req, res)
-{
-    let query1 = "SELECT * FROM Books;";
-    db.pool.query(query1, function(error, results){
+// DELETE TRANSACTION
+app.delete('/delete-transaction/:transactionID', function(req,res,next){
+    let data = req.body;
+    let deleteTransaction = `DELETE FROM Transactions WHERE transactionID = ?`;
+
+          db.pool.query(deleteTransaction, [req.params.transactionID], function(error, rows, fields){
+              if (error) {
+              console.log(error);
+              res.sendStatus(400);
+              }
+              else
+              {
+                  db.pool.query(deleteTransaction, [req.params.transactionID], function(error, rows, fields) {
+                      if (error) {
+                          console.log(error);
+                          res.sendStatus(400);
+                      } else {
+                          res.sendStatus(204);
+                      }
+                  })
+              }
+  })});
+
+
+// ADD TRANSACTION
+app.post('/add-transaction-form', function(req, res) {
+
+    let data = req.body;
+    let query1 = `INSERT INTO Transactions (transactionID, bookID, patronID, transactionDate, transactionType) VALUES (?, ?, ?, ?, ?)`;
+    // Execute the query with parameter values
+    db.pool.query(query1, [data.transactionID, data.bookID, data.patronID, data.transactionDate, data.transactionType], function(error, rows, fields) {
         if (error) {
-        res.status(500).send('Database error: ' + error.message);
+            console.log(error);
+            res.sendStatus(400);
         } else {
-        res.render('books', { data: results });
+            res.redirect('/transactions');
         }
     });
 });
 
-// Display Holds Table
+
+// DISPLAY HOLDS TABLE
 app.get('/holds', function(req, res)
 {
         let query1 = "SELECT * FROM Holds;";
@@ -85,10 +185,9 @@ app.get('/holds', function(req, res)
     });
 });
 
-// DELETES HOLD
+// DELETE HOLD
 app.delete('/delete-hold/:holdID', function(req,res,next){
     let data = req.body;
-    console.log(req.params.holdID);
     let deleteHold = `DELETE FROM Holds WHERE holdID = ?`;
 
           db.pool.query(deleteHold, [req.params.holdID], function(error, rows, fields){
@@ -109,8 +208,8 @@ app.delete('/delete-hold/:holdID', function(req,res,next){
               }
   })});
 
-  // ADDS HOLD
-  app.post('/add-hold-form', function(req, res) {
+// ADD HOLD
+app.post('/add-hold-form', function(req, res) {
 
     let data = req.body;
     let query1 = `INSERT INTO Holds (holdID, bookID, datePlaced, posInQueue, status, notificationPref) VALUES (?, ?, ?, ?, ?, ?)`;
@@ -126,7 +225,7 @@ app.delete('/delete-hold/:holdID', function(req,res,next){
     });
 });
 
-// UPDATES HOLD
+// UPDATE HOLD
 app.put('/update-hold-form', function(req,res,next){
     let data = req.body;
 
@@ -156,7 +255,20 @@ app.put('/update-hold-form', function(req,res,next){
               }
   })});
 
-// DELETES BOOK
+//DISPLAY BOOKS TABLE
+app.get('/books', function(req, res)
+{
+    let query1 = "SELECT * FROM Books;";
+    db.pool.query(query1, function(error, results){
+        if (error) {
+        res.status(500).send('Database error: ' + error.message);
+        } else {
+        res.render('books', { data: results });
+        }
+    });
+});
+
+// DELETE BOOK
 app.delete('/delete-book/:bookID', function(req,res){
     console.log(req.params.bookID);
     let deleteBook = `DELETE FROM Books WHERE bookID = ?`;
@@ -178,14 +290,14 @@ app.delete('/delete-book/:bookID', function(req,res){
               }
   })});
 
-  // ADDS BOOK
+  // ADD BOOK
   app.post('/add-book-form', function(req, res) {
     console.log('Received data:', req.body);
     let data = req.body;
-    let query1 = `INSERT INTO Books (title, author, subject, location) VALUES (?, ?, ?, ?)`;
+    let query1 = `INSERT INTO Books (bookID, title, author, subject, location) VALUES (?, ?, ?, ?, ?)`;
 
     // Execute the query with parameter values
-    db.pool.query(query1, [data.title, data.author, data.subject, data.location], function(error, rows, fields) {
+    db.pool.query(query1, [data.bookID, data.title, data.author, data.subject, data.location], function(error, rows, fields) {
         if (error) {
             console.log(error);
             res.sendStatus(400);
@@ -195,21 +307,40 @@ app.delete('/delete-book/:bookID', function(req,res){
     });
 });
 
+// UPDATE BOOK
+app.put('/update-book-form', function(req,res,next){
+    let data = req.body;
 
-// Display Patron_Holds Page
-app.get('/add_holds', function(req, res)
-{
-        let query1 = "SELECT * FROM Patron_Holds;";
-    db.pool.query(query1, function(error, results){
-        if (error) {
-        res.status(500).send('Database error: ' + error.message);
-        } else {
-        res.render('patron_holds', { data: results });
-        }
-    });
-});
+    let bookID = parseInt(data.bookID);
+    let title = data.title;
+    let author = data.author;
+    let subject = data.subject;
+    let location = data.location;
 
-// Display Patron Holds Table
+    let queryUpdateBook = `UPDATE Books SET title = ?, author = ?, subject = ?, location = ? WHERE bookID = ?`;
+
+          db.pool.query(queryUpdateBook, [title, author, subject, location, bookID], function(error, rows, fields){
+              if (error) {
+              console.log(error);
+              res.sendStatus(400);
+              }
+              else
+              {
+                let selectQuery = 'SELECT * FROM Books WHERE bookID = ?'
+                db.pool.query(selectQuery, [bookID], function(error, rows, fields) {
+
+                      if (error) {
+                          console.log(error);
+                          res.sendStatus(400);
+                      } else {
+                          res.send(rows);
+                      }
+                  })
+              }
+  })});
+
+
+// DISPLAY PATRON_HOLDS TABLE
 app.get('/patron_holds', function(req, res)
 {
         let query1 = "SELECT * FROM Patron_Holds;";
@@ -222,10 +353,48 @@ app.get('/patron_holds', function(req, res)
     });
 });
 
+// DELETE PATRON_HOLD
+app.delete('/delete-patron-hold/:patron_holdID', function(req,res,next){
+    let data = req.body;
+    let deletePatronHold = `DELETE FROM Patron_Holds WHERE patron_holdID = ?`;
+
+          db.pool.query(deletePatronHold, [req.params.patron_holdID], function(error, rows, fields){
+              if (error) {
+              console.log(error);
+              res.sendStatus(400);
+              }
+              else
+              {
+                  db.pool.query(deletePatronHold, [req.params.patron_holdID], function(error, rows, fields) {
+                      if (error) {
+                          console.log(error);
+                          res.sendStatus(400);
+                      } else {
+                          res.sendStatus(204);
+                      }
+                  })
+              }
+  })});
+
+// ADD PATRON_HOLDS
+app.post('/add-patron_hold-form', function(req, res) {
+
+    let data = req.body;
+    let query1 = `INSERT INTO Patron_Holds (patron_holdID, name, title, transactionDate) VALUES (?, ?, ?, ?)`;
+    // Execute the query with parameter values
+    db.pool.query(query1, [data.patronID, data.name, data.address, data.email, data.phone], function(error, rows, fields) {
+        if (error) {
+            console.log(error);
+            res.sendStatus(400);
+        } else {
+            res.redirect('/patron_holds');
+        }
+    });
+});
+
 /*
     LISTENER
 */
 app.listen(PORT, function(){
     console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
 });
-
