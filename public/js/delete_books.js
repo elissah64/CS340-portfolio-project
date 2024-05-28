b88f4259b4dc244a4ddcab8e74745e171facd76d
degiwhlo2co <147834857+degiwhlo2co@users.noreply.github.com>
2024-05-28 18:24:16 -0400

    Create delete_books.js instead of books.js for clarity, updated code to be functional

---
+++ public/js/delete_books.js
@@ -0,0 +1,21 @@
+// Citation for the following function
+// Date: 5/22/2024
+// Based on:
+// Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app
+
+function deleteBook(bookID) {
+    const data = {id: bookID}
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("DELETE", `/delete-book/${bookID}`, true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 204) {
+            location.reload()
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 204) {
+            console.log("There was an error with the input.")
+        }
+    }
+    xhttp.send(JSON.stringify(data));
+  }
